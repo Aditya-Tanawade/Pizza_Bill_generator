6a52b1cd964ba7cc30d379258823f75900315017
Aditya Tanawade <tanawade.aditya04@gmail.com>
2024-05-31 14:10:24 +0530

    Added Readme

--- src/Pizaa.java
+++ src/Pizaa.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Pizaa {
+public class Pizaa {
+
+
+    private int price;
+    private boolean veg;
+
+
+    public void pizza_Menu_Card(){
+        System.out.println("Veg Chesse pizza - 100/-");
+        System.out.println("Veg Chesse pizza - 200/-");
+        System.out.println("Veg Chesse pizza - 200/-");
+
+    }
+
+
+
+
+    public void getPizzaPrice(){
+        System.out.println(this.price);
+    }
+
 }
